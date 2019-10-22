74641d825dd8547cab67ab9096b79a49c3390402
Ryan Layer <ryan.layer@gmail.com>
2019-10-22 10:51:02 -0600

    make work for more types of chars

--- file_io.py
+++ file_io.py
@@ -2,7 +2,7 @@
 def get_file_columns(file_name, col_nums):
     cols = []
 
-    for l in open(file_name):
+    for l in open(file_name, encoding = "ISO-8859-1"):
         A = l.rstrip().split()
         col = []
         for i in col_nums:
